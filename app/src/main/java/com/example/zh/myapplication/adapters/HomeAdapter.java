fb7614cf532e4d28b4e5cdcf97e1e8d9c215d8fc
JokerCodenewbie <togetemail2014@163.com>
2016-08-02 18:18:37 +0800

    RecyclerView practise

---
+++ app/src/main/java/com/example/zh/myapplication/adapters/HomeAdapter.java
@@ -0,0 +1,32 @@
+package com.example.zh.myapplication.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by ZH on 2016/8/2.
+ */
+public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private LayoutInflater mLayoutInflater;
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/zh/myapplication/views/HomeActivity.java
@@ -0,0 +1,10 @@
+package com.example.zh.myapplication.views;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZH on 2016/8/2.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+}
